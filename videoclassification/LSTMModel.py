46aed6c834f6bfa87c44341e4443a86b6e74dd5f
martina <martina.ticha@blindspot.ai>
2019-05-30 14:17:30 +0200

    callback added

--- videoclassification/LSTMModel.py
+++ videoclassification/LSTMModel.py
@@ -5,55 +5,55 @@ from sklearn.base import BaseEstimator, ClassifierMixin
 from LSTMNetwork import LSTMNetwork
 from generators import train_generator, predict_generator
 
-BBOX_TRAIN = "../data/bbox_train_"
-SIMPLE = "../data/simple_data_set_test"
-
-# DATA_PATH_TRAIN = SIMPLE
-SEQUENCE_LEN = 9
-MODEL = "model"
-LABELS = "labels"
-GPU = "7"
+INIT_LR = 0.004
+BS = 2
 
 
 class LSTMModel(BaseEstimator, ClassifierMixin):
-    def __init__(self, num_of_classes, training_samples, test_samples, EPOCHS=None):
+    def __init__(
+            self, num_of_classes, training_samples, test_samples, sequence_len=9, EPOCHS=None):
         self.num_of_classes = num_of_classes
-
-        self.model = LSTMNetwork.build(width=64, height=64, depth=3, sequence_len=SEQUENCE_LEN,
-                                       num_of_classes=num_of_classes)
         self.EPOCHS = EPOCHS
         self.training_samples = training_samples
         self.test_samples = test_samples
-
-        self.INIT_LR = 0.004
         # self.EPOCHS = 500
-        self.BS = 30
+        self.SEQUENCE_LEN = sequence_len
+
+        self.model = LSTMNetwork.build(
+            width=64,
+            height=64,
+            depth=3,
+            sequence_len=self.SEQUENCE_LEN,
+            num_of_classes=num_of_classes
+        )
 
         print("[INFO] train data size: " + str(self.training_samples))
         print("[INFO] test data size: " + str(self.test_samples))
-        print("[INFO] steps per epoch: " + str(self.training_samples / self.BS))
+        print("[INFO] steps per epoch: " + str(self.training_samples / BS))
 
-        self.tensorboard = TensorBoard(log_dir="logs/{}".format(self.INIT_LR))
+        self.tensorboard = TensorBoard(log_dir="logs/{}".format(INIT_LR))
 
         print("[INFO] training network...")
-        opt = SGD(lr=self.INIT_LR, decay=self.INIT_LR / num_of_classes)
+        opt = SGD(lr=INIT_LR, decay=INIT_LR / num_of_classes)
         self.model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["categorical_accuracy"])
 
     def fit(self, trainX, trainY, fit_params):
         print("[INFO] fitting..")
+        print(len(trainX))
+        print(trainY)
         testX = fit_params['testX']
         testY = fit_params['testY']
         self.label_to_folder = fit_params['label_to_folder']
         self.model.fit_generator(
-            generator=train_generator(trainX, trainY, self.BS, self.num_of_classes, self.label_to_folder),
-            steps_per_epoch=self.training_samples / (self.BS),
+            generator=train_generator(trainX, trainY, BS, self.num_of_classes, self.label_to_folder),
+            steps_per_epoch=self.training_samples / (BS),
             validation_data=train_generator(
                 testX,
                 testY,
-                self.BS,
+                BS,
                 self.num_of_classes,
                 self.label_to_folder),
-            validation_steps=self.training_samples / self.BS,
+            validation_steps=self.training_samples / BS,
             epochs=self.EPOCHS,
             verbose=1,
             callbacks=[self.tensorboard]
@@ -65,7 +65,7 @@ class LSTMModel(BaseEstimator, ClassifierMixin):
 
     def score(self, X, y, **kwargs):
         _, acc = self.model.evaluate_generator(
-            generator=train_generator(X, y, self.BS, self.num_of_classes, self.label_to_folder), steps=1)
+            generator=train_generator(X, y, BS, self.num_of_classes, self.label_to_folder), steps=1)
         return acc
 
     def get_model(self):
