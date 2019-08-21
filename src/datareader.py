e06df8c02c8ee8656a0ac707624eeeed058939c2
martina <martina.ticha@blindspot.ai>
2019-08-21 17:30:24 +0200

    path changed

--- src/datareader.py
+++ src/datareader.py
@@ -151,7 +151,7 @@ class DataReader:
         unique_cameras = 0
         identities = os.listdir(data_path)
         identities.sort()
-        for identity in identities[0:2]:
+        for identity in identities:
             num_of_videos_for_identity, identity_data = _load_one_identity(data_path, identity)
 
             if num_of_videos_for_identity >= MIN_NUM_OF_VIDEOS:
