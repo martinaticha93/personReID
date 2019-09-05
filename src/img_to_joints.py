97684f586eef52ffd38122ee5f318f5a89067faa
martina <martina.ticha@blindspot.ai>
2019-09-05 13:44:53 +0200

    Keypoints computed online

--- src/img_to_joints.py
+++ src/img_to_joints.py
@@ -9,6 +9,7 @@ import argparse
 import os
 import torch
 import timeit
+import scipy.misc
 
 
 class PP:
@@ -29,56 +30,52 @@ class PP:
         model = model.to(self.device)
         self.processor = openpifpaf.decoder.factory_from_args(args_openpifpaf, model)
 
-    def process_img(self, img_name: str, identity: str, output_dir: str, keypts_dir: str, edges_dir: str):
-        # img = cv2.imread(os.path.join(edges_dir, identity, img_name))
-        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    def process_img(self, img_name: str, identity: str, output_dir: str, input_dir: str, edges_dir: str):
+        img_name_short = img_name.split('_')[0] + '.jpg'
+        img = cv2.imread(os.path.join(input_dir, identity, img_name_short))
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        scipy.misc.imsave(os.path.join('/media/martina/Data/School/CTU/thesis/data/mars_edges_with_kpts_selected_20_64x64', img_name_short), img)
 
-        try:
-            img_edges = cv2.imread(os.path.join(edges_dir, identity, img_name))
-            img_edges = img_edges[:, 63:192, :]
-            # processed_image_cpu = openpifpaf.transforms.image_transform(img)
-            # processed_image = processed_image_cpu.contiguous().to(self.device, non_blocking=True)
-            # fields = self.processor.fields(torch.unsqueeze(processed_image, 0))[0]
-            # keypoint_sets, scores = self.processor.keypoint_sets(fields)
+        img_edges = cv2.imread(os.path.join(edges_dir, identity, img_name))
+        # img_edges = img_edges[:, 63:192, :]
+        processed_image_cpu = openpifpaf.transforms.image_transform(img)
+        processed_image = processed_image_cpu.contiguous().to(self.device, non_blocking=True)
+        fields = self.processor.fields(torch.unsqueeze(processed_image, 0))[0]
+        keypoint_sets, scores = self.processor.keypoint_sets(fields)
 
-            skeleton_painter = openpifpaf.show.KeypointPainter(show_box=False,
-                                                               color_connections=True,
-                                                               markersize=1,
-                                                               linewidth=6)
+        skeleton_painter = openpifpaf.show.KeypointPainter(show_box=False,
+                                                           color_connections=True,
+                                                           markersize=1,
+                                                           linewidth=6)
 
-            with openpifpaf.show.canvas(show=False) as ax:
-                try:
-                    # arg_max_score = np.argmax(scores)
-                    # keypoint_set, score = keypoint_sets[arg_max_score, :, :], scores[arg_max_score]
+        with openpifpaf.show.canvas(show=False) as ax:
+            try:
+                arg_max_score = np.argmax(scores)
+                keypoint_set, score = keypoint_sets[arg_max_score, :, :], scores[arg_max_score]
 
-                    # pure joints
-                    # img = np.zeros(img.shape, dtype=np.uint8)
-                    # ax.imshow(img)
+                # # pure joints
+                # img = np.zeros(img.shape, dtype=np.uint8)
+                # ax.imshow(img)
+                # skeleton_painter.keypoints(ax, [keypoint_set], scores=[score])
+                # ax.figure.savefig(os.path.join(output_dir,  identity, f'{img_name[:-4]}_{score}.jpg'))
 
-                    # joints with edges
-                    ax.imshow(img_edges)
-                    # skeleton_painter.keypoints(ax, [keypoint_set], scores=[score])
-                    # ax.figure.savefig(
-                    #     os.path.join(output_dir, 'joints_edges_original', identity, f'{img_name[:-4]}_{score}.jpg'))
+                # joints with edges
+                ax.imshow(img_edges)
+                keypoint_set[:, 0] = keypoint_set[:, 0] / 4 + 16
+                keypoint_set[:, 1] = keypoint_set[:, 1] / 4
+                skeleton_painter.keypoints(ax, [keypoint_set], scores=[score])
+                ax.figure.savefig(os.path.join(output_dir, identity, f'{img_name[:-4]}_{score}.jpg'))
 
-                    # keypoint_set = [score, keypoint_set]
-
-                    # np.save(os.path.join(output_dir, 'key_points', identity, img_name), keypoint_set)
-                    keypts_name = img_name.split('_')[0] + '.jpg.npy'
-
-                    [score, keypoint_set] = np.load(os.path.join(keypts_dir, identity, keypts_name ))
-                    skeleton_painter.keypoints(ax, [keypoint_set], scores=[score])
-                    ax.figure.savefig(os.path.join(output_dir, identity, f'{img_name[:-4]}.jpg'))
-                except:
-                    print(f'[INFO]..no joints found for identity {identity} image {img_name}')
-        except:
-            print(f'[INFO]..image {img_name} for identity {identity} could not have been tranformed')
+                # keypoint_set = [score, keypoint_set]
+                # np.save(os.path.join(output_dir, 'key_points', identity, img_name), keypoint_set)
+            except:
+                print(f'[INFO]..no joints found for identity {identity} image {img_name}')
 
 
 if __name__ == '__main__':
-    output_dir = '/media/martina/Data/School/CTU/thesis/data/mars_edges_with_kpts_selected_20_256x256'
-    keypts_dir = '/media/martina/Data/School/CTU/thesis/data/key_points'
-    edges_dir = '/media/martina/Data/School/CTU/thesis/data/mars_edges_selected_20_256x256'
+    output_dir = '/media/martina/Data/School/CTU/thesis/data/mars_edges_with_kpts_selected_20_64x64'
+    input_dir = '/media/martina/Data/School/CTU/thesis/data/mars'
+    edges_dir = '/media/martina/Data/School/CTU/thesis/data/mars_edges_selected_20_64x64'
 
     # shutil.rmtree(os.path.join(output_dir, 'joints_edges'))
     # shutil.rmtree(os.path.join(output_dir, 'joints'))
@@ -88,16 +85,17 @@ if __name__ == '__main__':
     # os.mkdir(os.path.join(output_dir, 'joints'))
     # os.mkdir(os.path.join(output_dir, 'key_points'))
 
-    all_identities = os.listdir(edges_dir)
+    all_identities = os.listdir(input_dir)
     processed_identities = os.listdir(output_dir)
     # [all_identities.remove(identity) for identity in processed_identities]
     all_identities.sort()
     p = PP()
     for identity in all_identities:
+        print(identity)
         os.mkdir(os.path.join(output_dir, identity))
         images = os.listdir(os.path.join(edges_dir, identity))
         images.sort()
 
         for image in images:
-            p.process_img(image, identity, output_dir, keypts_dir, edges_dir)
+            p.process_img(image, identity, output_dir, input_dir, edges_dir)
         print(f'[INFO]..{identity} loaded')
