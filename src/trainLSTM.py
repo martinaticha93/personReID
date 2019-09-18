9d3803d56bd711849d7d1843da89843eb3948e5b
martina <martina.ticha@blindspot.ai>
2019-09-18 10:58:17 +0200

    kpts_edges and edges training split into two methods

--- src/trainLSTM.py
+++ src/trainLSTM.py
@@ -19,18 +19,21 @@ SERVER_MARS_EDGES_KEYPTS_20 = "../data/mars_edges_with_kpts_selected_20_64x64"
 LOCAL_MARS_EDGES_20 = '/media/martina/Data/School/CTU/thesis/data/mars_edges_selected_20'
 LOCAL_MARS_KEYPTS_20 = '/media/martina/Data/School/CTU/thesis/data/mars_key_points_selected_20'
 LOCAL_MARS_EDGES_POSTPRO_20 = '/media/martina/Data/School/CTU/thesis/data/mars_edges_postpro_selected_20'
-LOCAL_MARS_EDGES_KEYPTS_20 = '/media/martina/Data/School/CTU/thesis/data/mars_edges_with_kpts_selected_20_200x200'
+LOCAL_MARS_EDGES_KEYPTS_20 = '/media/martina/Data/School/CTU/thesis/data/mars_edges_with_kpts_selected_20_64x64'
 
 MARS_EDGES_LOCAL = '/media/martina/Data/School/CTU/thesis/data/mars_joints/joints_edges'
 MARS_LOCAL = '/media/martina/Data/School/CTU/thesis/data/mars'
 
-DATA_PATH_TRAIN = LOCAL_MARS_KEYPTS_20
+DATA_PATH_TRAIN = LOCAL_MARS_EDGES_KEYPTS_20
 MODEL = "model"
 LABELS = "labels"
 TEST_X_KEY_POINTS = 'testX_key_points'
 TEST_Y_KEY_POINTS = 'testY_key_points'
 TEST_X_EDGES = 'testX_edges'
 TEST_Y_EDGES = 'testY_edges'
+TEST_X_EDGES_AND_KPTS = 'testX_edges_kpts'
+TEST_Y_EDGES_AND_KPTS = 'testY_edges_kpts'
+
 GPU = "6"
 
 
@@ -86,34 +89,32 @@ def _train_on_edges():
     model.fit()
     return model.get_model(), label_to_folder
 
+def _train_on_edges_and_kpts():
+    print('[INFO] edges and key points training...')
+    print("[INFO] obtaining data...")
+
+    trainX, trainY, testX, testY, num_of_classes, label_to_folder, groups_train = DataReader.prepare_data(
+        DATA_PATH_TRAIN, load_edges
+    )
+
+    f = open(TEST_X_EDGES_AND_KPTS, "wb")
+    f.write(pickle.dumps(testX))
+
+    f = open(TEST_Y_EDGES_AND_KPTS, "wb")
+    f.write(pickle.dumps(testY))
+
+    model = EdgesModel(trainX, trainY, testX, testY, num_of_classes, label_to_folder)
+    model.fit()
+    return model.get_model(), label_to_folder
+
 
 def train():
+    if 'edges_with_kpts' in DATA_PATH_TRAIN:
+        return _train_on_edges_and_kpts()
     if 'key' in DATA_PATH_TRAIN:
         return _train_on_key_points()
     elif 'edges' in DATA_PATH_TRAIN:
         return _train_on_edges()
-
-    tuned_params = {
-        "EPOCHS": [100],
-        "INIT_LR": [0.001, 0.0004],
-        "DECAY_FACTOR": [0.8]
-    }
-
-    # split that is used for cross validation in grid search - for each split there's a run of the alg
-    # seems to be useless because this way it splits twice
-    # cv = list(GroupShuffleSplit(n_splits=3).split(trainX, trainY, groups_train))
-    # gs = GridSearchCV(model, tuned_params, cv=cv)
-    # fit_params = {
-    #     'label_to_folder': label_to_folder,
-    #     'testX': testX,
-    #     'testY': testY
-    # }
-    #
-    # gs.fit(trainX, trainY, fit_params=fit_params)
-
-    # print(sorted(gs.cv_results_.keys()))
-    # print(gs.best_params_)
-    return None, None
 
 
 if __name__ == '__main__':
