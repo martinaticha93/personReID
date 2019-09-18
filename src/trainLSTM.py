38b7bcb1ac32eef5c9c4dd08f465a44028505986
martina <martina.ticha@blindspot.ai>
2019-09-18 14:38:42 +0200

    code cleaning

--- src/trainLSTM.py
+++ src/trainLSTM.py
@@ -25,7 +25,9 @@ MARS_EDGES_LOCAL = '/media/martina/Data/School/CTU/thesis/data/mars_joints/joint
 MARS_LOCAL = '/media/martina/Data/School/CTU/thesis/data/mars'
 
 DATA_PATH_TRAIN = SERVER_MARS_EDGES_20
-MODEL = "model"
+MODEL_k = "model_k"
+MODEL_e = "model_e"
+MODEL_ke = "model_ke"
 LABELS = "labels"
 TEST_X_KEY_POINTS = 'pickles/testX_k'
 TEST_Y_KEY_POINTS = 'pickles/testY_k'
@@ -35,6 +37,7 @@ TEST_X_EDGES_AND_KPTS = 'pickles/testX_ek'
 TEST_Y_EDGES_AND_KPTS = 'pickles/testY_ek'
 
 GPU = "6"
+
 
 class TestCallback(Callback):
     def __init__(self, test_data, label_to_folder):
@@ -84,7 +87,7 @@ def _train_on_key_points():
 
     model = KeyPtsModel(trainX, trainY, testX, testY, num_of_classes, label_to_folder)
     model.fit()
-    return model.get_model(), label_to_folder
+    model.get_model().save(MODEL_k)
 
 
 def _train_on_edges():
@@ -108,7 +111,8 @@ def _train_on_edges():
 
     model = EdgesModel(trainX, trainY, testX, testY, num_of_classes, label_to_folder)
     model.fit()
-    return model.get_model(), label_to_folder
+    model.get_model().save(MODEL_e)
+
 
 def _train_on_edges_and_kpts():
     print('[INFO] edges and key points training...')
@@ -123,35 +127,23 @@ def _train_on_edges_and_kpts():
     f = open(TEST_Y_EDGES_AND_KPTS, "wb")
     f.write(pickle.dumps(testY))
 
-    trainX = pickle.loads(open("trainX_k", "rb").read())
-    trainY = pickle.loads(open("trainY_k", "rb").read())
-    testX = pickle.loads(open("testX_k", "rb").read())
-    testY = pickle.loads(open("testY_k", "rb").read())
-    num_of_classes = pickle.loads(open("num_of_classes_k", "rb").read())
-    label_to_folder = pickle.loads(open("label_to_folder_k", "rb").read())
-    groups_train = pickle.loads(open("groups_train", "rb").read())
-
     model = EdgesModel(trainX, trainY, testX, testY, num_of_classes, label_to_folder)
     model.fit()
-    return model.get_model(), label_to_folder
+    model.get_model().save(MODEL_ke)
 
 
 def train():
     if 'edges_with_kpts' in DATA_PATH_TRAIN:
-        return _train_on_edges_and_kpts()
-    if 'key' in DATA_PATH_TRAIN:
-        return _train_on_key_points()
+        _train_on_edges_and_kpts()
+    elif 'key' in DATA_PATH_TRAIN:
+        _train_on_key_points()
     elif 'edges' in DATA_PATH_TRAIN:
-        return _train_on_edges()
+        _train_on_edges()
 
 
 if __name__ == '__main__':
     start = int(round(time.time()))
     with tf.device('/gpu:' + GPU):
-        model, label_to_folder = train()
+        train()
         end = int(round(time.time()))
         print("[INFO] the training took..." + str(end - start) + "second")
-        model.save(MODEL)
-        f = open(LABELS, "wb")
-        f.write(pickle.dumps(label_to_folder))
-        f.close()
