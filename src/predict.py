46748b8ceb776d980e2cf2d7065c218faffe4a30
martina <martina.ticha@blindspot.ai>
2019-09-23 10:43:45 +0200

    predict notebook added

--- src/predict.py
+++ src/predict.py
@@ -1,22 +1,23 @@
 import pickle
-import numpy as np
 
+import numpy as np
 from keras.models import load_model
 
 from generators import predict_generator
-from trainLSTM import TEST_X_KEY_POINTS, TEST_Y_KEY_POINTS
 
 DATA_PATH = "../data/simple_data_set_test/0078"
-MODEL = "model"
+MODEL = "models/model_e_e_0_"
 LABELS = "labels"
+TEST_X = 'pickles/testX_e_e_0_'
+TEST_Y = 'pickles/testY_e_e_0_'
 
 
 def predict_on_test_data():
     print("[INFO] loading network and label map...")
     model = load_model(MODEL)
-    label_to_folder = pickle.loads(open(LABELS, "rb").read())
-    testX = pickle.loads(open(TEST_X_KEY_POINTS, "rb").read())
-    testY = pickle.loads(open(TEST_Y_KEY_POINTS, "rb").read())
+    # label_to_folder = pickle.loads(open(LABELS, "rb").read())
+    testX = pickle.loads(open(TEST_X, "rb").read())
+    testY = pickle.loads(open(TEST_Y, "rb").read())
 
     print("[INFO] predicting")
     return np.argmax(model.predict_generator(predict_generator(testX), steps=testX.shape[0]), axis=1), testY
@@ -25,4 +26,3 @@ def predict_on_test_data():
 if __name__ == '__main__':
     predictions, testY = predict_on_test_data()
     stop = 5
-
