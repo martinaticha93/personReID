0b867aea0898fe9c9b07961e35758c69b92676a8
martina <martina.ticha@blindspot.ai>
2019-09-27 16:39:02 +0200

    code cleaning

--- src/EdgesModel.py
+++ src/EdgesModel.py
@@ -23,7 +23,7 @@ class EdgesModel(BaseEstimator, ClassifierMixin):
         self.TRAINING_SAMPLES = len(trainX)
         self.TEST_SAMPLES = len(testX)
 
-        self.INIT_LR = 0.001
+        self.INIT_LR = 0.005
         self.EPOCHS = 1000
         self.BS = 30
 
